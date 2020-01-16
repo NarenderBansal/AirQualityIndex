7fa2bd727403ad8d88fe7efe7029d40082d8a699
Narendra <59466697+NarenderBansal@users.noreply.github.com>
2020-01-16 15:16:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template, url_for
+import pickle
+
+# load the model from disk
+app = Flask(__name__)
+model = pickle.load(open('Randonforest.pkl','rb'))
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+	
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = np.array(int_features)
+    data=final_features.reshape(1,-1)
+    predictions=model.predict(data)
+    output = round(predictions[0], 2)
+    return render_template('home.html', prediction_text='AQI for Delhi {}'.format(output))
+	
+if __name__ == '__main__':
+    app.run(debug=True)
